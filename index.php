abe749605b1fe0fcca2403e8a5bf8a91ce55068f
Faker <76863899+fakerxcode@users.noreply.github.com>
2022-08-14 21:01:11 +0800

    Detail file
    My ongoing personal file

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en" xml:lang="">
+  <head>
+    <title>Faker &#128151;</title>
+    <link rel="icon" type="image/x-icon" href="https://ro10.ched.gov.ph/wp-content/uploads/2022/08/favicon.ico">
+
+    <meta charset="UTF-8">
+    <meta name="description" content="Faker website">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="author" content="Faker">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="30">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css" integrity="sha512-XWTTruHZEYJsxV3W/lSXG1n3Q39YIWOstqvmFsdNEEQfHoZ6vm6E9GK2OrF6DSJSpIbRbi+Nn0WDPID9O7xB2Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  </head> 
+  <body>
+
+    <div class="container my-4">
+
+      <div class="row">
+        <div class="col-6">
+          <div class="">
+            <img src="https://picsum.photos/200/200?random=1" class="rounded shadow" alt="Random image">
+          </div>
+          <p title='Faker' class='fs-2 fw-semibold mb-0 lh-1 my-2'>
+            Faker 
+            <div class='text-muted'>
+              @fakerxcode
+            </div>
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-8">
+          <p title='About me' class="fw-normal">
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+          </p>
+        </div>
+      </div>
+
+      <div class="row justify-content-center">
+        <div class="col-m-6 text-center">
+          <p>
+            <h1 class="display-6">Background & Experience</h1>
+          </p>
+        </div>
+      </div>
+
+      <div class="row gy-4 justify-content-center">
+        <div class="col-md-3 col-sm-4 col-4">
+          <p title='Frontend' class="fw-normal text-center">
+            Frontend
+          </p>
+          <div class="card border-0 shadow">      
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><i class="fa-brands fa-react"></i> React JS</li>
+              <li class="list-group-item"><i class="fa-brands fa-vuejs"></i> Vuejs</li>
+              <li class="list-group-item"><i class="fa-brands fa-angular"></i> Angular</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-4 col-4">
+          <p title='Backend' class="fw-normal text-center">
+            Backend
+          </p>
+          <div class="card border-0 shadow">      
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><i class="fa-brands fa-laravel"></i> Laravel</li>
+              <li class="list-group-item"><i class="fa-brands fa-python"></i> Django</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-4 col-4">
+          <p title='Database' class="fw-normal text-center">
+            Database
+          </p>
+          <div class="card border-0 shadow">      
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><i class="fa-solid fa-database"></i> MySQL</li>
+              <li class="list-group-item"><i class="fa-solid fa-database"></i> NoSQL</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/js/bootstrap.min.js" integrity="sha512-8Y8eGK92dzouwpROIppwr+0kPauu0qqtnzZZNEF8Pat5tuRNJxJXCkbQfJ0HlUG3y1HB3z18CSKmUo7i2zcPpg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  </body>
+</html
