d80993a8ff0be2de51833daab769f089a6d98c21
Ghodie Madrona <gbtm.workspace@gmail.com>
2023-01-05 11:29:40 +0800

    rename documents.tsx to doucment.tsx

--- next.config.js
+++ next.config.js
@@ -2,14 +2,16 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  experimental: {
-    images: { // This will cause an error
-      allowFutureImage: true,
-    },
-  },
+  // experimental: {
+  //   images: { // This will cause an error
+  //     allowFutureImage: true,
+  //   },
+  // },
 
   // Potential new config flag:
-  disableExperimentalFeaturesWarning: true
+  // disableExperimentalFeaturesWarning: true
+
+  /* config options here */
 
 }
 
