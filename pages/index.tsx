ab329c663950649bee4da9dee7addcfa4735adf5
Ghodie Madrona <gbtm.workspace@gmail.com>
2023-01-06 02:20:30 +0800

    update about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -78,8 +78,9 @@ const IndexPage = () => (
             <img className='w-100 aspect-square rounded-lg' src='https://picsum.photos/400/300?random=1' />
           </div>
           <div className='bg-gray-100 rounded-lg p-4 w-96 text-sm justify-self-center md:justify-self-auto shadow'>
-            I am a full stack developer with over 3 years of experience of building scalable applications. { } <br />
+            I am a full stack developer with over 3 years of experience of building scalable applications. { } <br /><br />
             I used my spare time exploring some new tech tools to improve my skills and I enjoy solving problems. Being a developer is very challenging, so the best way to cope up with this is to take a break and do some stuff like walk, take a snack and play video games.
+            I love to read manga and manhwa in my past time and like to create one, but I don't have the proper tools and skills. 😂
           </div>
         </div>
       </section>
