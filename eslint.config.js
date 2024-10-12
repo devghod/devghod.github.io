6a69f44a9dce8569b1b6d5b01462237284a549e6
devghod <gbtm.workspace@gmail.com>
2024-10-12 22:47:47 +0800

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   rules: {
-    'no-console': 'warn',
-    'no-unused-vars': 'warn',
+    // 'no-console': 'warn',
+    // 'no-unused-vars': 'warn',
   },
   // "parserOptions": {
   //   "ecmaVersion": 2022
